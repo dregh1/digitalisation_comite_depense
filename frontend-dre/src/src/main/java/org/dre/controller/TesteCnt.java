31e97bdf90215ae86bc7f755824480be219554cc
Ash Lalunah <84842337+young-winner@users.noreply.github.com>
2024-04-26 15:13:32 +0100

    Add files via upload

---
+++ frontend-dre/src/src/main/java/org/dre/controller/TesteCnt.java
@@ -0,0 +1,190 @@
+package org.dre.controller;
+
+import io.quarkus.security.Authenticated;
+import jakarta.annotation.security.PermitAll;
+import jakarta.annotation.security.RolesAllowed;
+import jakarta.inject.Inject;
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.Context;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.core.SecurityContext;
+import org.dre.model.*;
+import org.dre.model.Periode;
+import org.dre.service.*;
+//import org.eclipse.microprofile.jwt.JsonWebToken;
+    //mila esorina reto
+//import org.keycloak.AuthorizationContext;
+//import org.keycloak.KeycloakPrincipal;
+//import org.keycloak.KeycloakSecurityContext;
+//import org.keycloak.representations.AccessToken;
+
+import java.util.List;
+
+@Path("/teste")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+@Authenticated
+public class TesteCnt {
+
+
+    @Inject
+    PeriodeService periodeService;
+    @Inject
+    DirectionService directionService;
+    @Inject
+    RubriqueService rubriqueService;
+    @Inject
+    ValidationService validationService;
+    @Inject
+    DetailDemandeService detailDemandeService;
+
+
+    @Inject
+    FournisseurService fournisseurService;
+
+    @Inject
+    DemandeService demandeService;
+
+    @Inject
+    SessionCdService sessionCdService;
+
+
+//    TEST CRUD
+    @GET
+    @Path("/periode/get")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getAllperiode() {
+        // Récupérer les données depuis PostgreSQL
+        List<Periode> sessionCds = periodeService.getAll ();
+        return Response.ok(sessionCds).build();
+    }
+
+    @GET
+    @Path("/demande/get")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getAlldemande() {
+        // Récupérer les données depuis PostgreSQL
+        List<Demande> demande = demandeService.getAll ();
+        return Response.ok(demande).build();
+    }
+
+    @GET
+    @Path("/fournisseur/get")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getAllFournisseur() {
+        // Récupérer les données depuis PostgreSQL
+        List<Fournisseur> rubriques = fournisseurService.getAll ();
+        return Response.ok(rubriques).build();
+    }
+
+
+
+
+    @POST
+    @Path("/post/create")
+    public Response createSessionCd(Periode sessionDd) {
+
+
+
+        periodeService.create(sessionDd);
+        return Response.status(Response.Status.CREATED).entity(sessionDd).build();
+    }
+
+    @POST
+    @Path("demande/create")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response createDemande(Demande demande) {
+        demandeService.create(demande);
+        return Response.status(Response.Status.CREATED).entity(demande).build();
+    }
+
+    //get id direction by name of direction
+    @GET
+    @Path("/getIdDir")
+    @PermitAll
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getIdDirByName(@QueryParam("nom") String nomDir) {
+
+        Long idDir = null;
+        Direction d= null;
+        if(nomDir!= null)
+        {
+            d = directionService.getIdDirByName(nomDir);
+        }
+
+
+
+        return Response.ok(d).build();
+    }
+    @POST
+    @Path("/getIdDir")
+    public Response createUser(String nomDir) {
+        Long idDir ;
+
+        if(nomDir!= null)
+        {
+            idDir = directionService.getIdDirByName(nomDir).getId();
+        }else idDir= null;
+
+        return Response.ok(idDir).build();
+    }
+//GET ALL SESSION
+    @GET
+    @Path("/session/get")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getAllSession() {
+        // Récupérer les données depuis PostgreSQL
+        List<SessionCd> session = sessionCdService.getAll ();
+        return Response.ok(session).build();
+    }
+//GET SESSION ACTIVE
+    @GET
+    @Path("/session/getByDir/{idDirection}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getSessionActive(@PathParam("idDirection") Integer idDirection) {
+        // Récupérer les données depuis PostgreSQL
+        SessionCd session = sessionCdService.getSessionActive(idDirection);
+        return Response.ok(session).build();
+    }
+//GET ALL DETAILDEMANDE
+    @GET
+    @Path("/detailDemande/get")
+    @RolesAllowed({"PRS","CDG","ACH"})
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getAllDetailDemande() {
+        // Récupérer les données depuis PostgreSQL
+        List<DetailDemande> detailDemande = detailDemandeService.getAll();
+        return Response.ok(detailDemande).build();
+    }
+    @GET
+    @Path("/detailDemande/{id}")
+    @RolesAllowed({"PRS","CDG","ACH"})
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getDetailDemande(@PathParam( "id") Long id) {
+        // Récupérer les données depuis PostgreSQL
+        DetailDemande detailDemande = detailDemandeService.getDetailDemandeById(id);
+        return Response.ok(detailDemande).build();
+    }
+    @GET
+    @Path("/validation/get")
+    @RolesAllowed({"PRS","CDG","ACH"})
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getAllValidation() {
+        // Récupérer les données depuis PostgreSQL
+        List<Validation> validation = validationService.getAll();
+        return Response.ok(validation).build();
+    }
+    @GET
+    @Path("/validation/{id}")
+    @RolesAllowed({"PRS","CDG","ACH"})
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getValidationById(@PathParam( "id") Long id) {
+        // Récupérer les données depuis PostgreSQL
+        Validation validation = validationService.getById(id);
+        return Response.ok(validation).build();
+    }
+
+}
+
+
---
+++ frontend-dre/src/src/main/java/org/dre/model/Brouillon.java
@@ -0,0 +1,177 @@
+package org.dre.model;
+
+import io.quarkus.hibernate.orm.panache.PanacheEntity;
+import jakarta.annotation.Nullable;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Brouillon  {
+    @Id
+ private   Long id ;
+ private  Integer idTitre ;
+ private    String  titre ;
+ private  String motif ;
+ private  double montantHt ;
+ private  String  typeReference ;
+ private  String  reference ;
+ private  boolean estRegularisation ;
+ private  String  comsPrescripteur ;
+ private  Integer idPeriode ;
+ private  String  periode ;
+ private  Integer idDirection ;
+ private  String  devise ;
+ private  Integer idFournisseur ;
+
+ private Integer idRubrique;
+ private String sousRubrique;
+
+ private String nomRubrique;
+ private  String  fournisseur;
+
+    public String getSousRubrique() {
+        return sousRubrique;
+    }
+
+    public void setSousRubrique(String sousRubrique) {
+        this.sousRubrique = sousRubrique;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Integer getIdTitre() {
+        return idTitre;
+    }
+
+    public void setIdTitre(Integer idTitre) {
+        this.idTitre = idTitre;
+    }
+
+    public String getTitre() {
+        return titre;
+    }
+
+    public void setTitre(String titre) {
+        this.titre = titre;
+    }
+
+    public String getMotif() {
+        return motif;
+    }
+
+    public void setMotif(String motif) {
+        this.motif = motif;
+    }
+
+    public double getMontantHt() {
+        return montantHt;
+    }
+
+    public void setMontantHt(double montantHt) {
+        this.montantHt = montantHt;
+    }
+
+    public String getTypeReference() {
+        return typeReference;
+    }
+
+    public void setTypeReference(String typeReference) {
+        this.typeReference = typeReference;
+    }
+
+    public String getReference() {
+        return reference;
+    }
+
+    public void setReference(String reference) {
+        this.reference = reference;
+    }
+
+    public boolean isEstRegularisation() {
+        return estRegularisation;
+    }
+
+    public void setEstRegularisation(boolean estRegularisation) {
+        this.estRegularisation = estRegularisation;
+    }
+
+    public String getComsPrescripteur() {
+        return comsPrescripteur;
+    }
+
+    public void setComsPrescripteur(String comsPrescripteur) {
+        this.comsPrescripteur = comsPrescripteur;
+    }
+
+    public Integer getIdPeriode() {
+        return idPeriode;
+    }
+
+    public void setIdPeriode(Integer idPeriode) {
+        this.idPeriode = idPeriode;
+    }
+
+    public String getPeriode() {
+        return periode;
+    }
+
+    public void setPeriode(String periode) {
+        this.periode = periode;
+    }
+
+    public Integer getIdDirection() {
+        return idDirection;
+    }
+
+    public void setIdDirection(Integer idDirection) {
+        this.idDirection = idDirection;
+    }
+
+    public String getDevise() {
+        return devise;
+    }
+
+    public void setDevise(String devise) {
+        this.devise = devise;
+    }
+
+    public Integer getIdFournisseur() {
+        return idFournisseur;
+    }
+
+    public void setIdFournisseur(Integer idFournisseur) {
+        this.idFournisseur = idFournisseur;
+    }
+
+    public Integer getIdRubrique() {
+        return idRubrique;
+    }
+
+    public void setIdRubrique(Integer idRubrique) {
+        this.idRubrique = idRubrique;
+    }
+
+    public String getNomRubrique() {
+        return nomRubrique;
+    }
+
+    public void setNomRubrique(String nomRubrique) {
+        this.nomRubrique = nomRubrique;
+    }
+
+    public String getFournisseur() {
+        return fournisseur;
+    }
+
+    public void setFournisseur(String fournisseur) {
+        this.fournisseur = fournisseur;
+    }
+}
