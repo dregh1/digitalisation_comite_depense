9a9b727957b46f1a1ae9f142091f28d1cdd4d8ab
Ash Lalunah <84842337+young-winner@users.noreply.github.com>
2024-05-08 06:07:57 -0700

    Add files via upload

---
+++ frontend-hasina/src/app/app.module.ts
@@ -0,0 +1,37 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { AuthenticationComponent } from './component/Authentication/authentication.component';
+import { ConsultationComponent } from './component/consultation/consultation.component';
+import { NotificationComponent } from './component/notification/notification.component';
+import { MainComponent } from './component/main/main.component';
+import { CreationPrescripteurComponent } from './component/CreationPrescripteur/CreationPrescripteur.component';
+import { CreationSession1Component } from './component/CreationSession/CreationSession.component';
+import { HeaderComponent } from './component/header/header.component';
+import { TestComponent } from './component/DetailParDemande/DetailParDemande.component';
+import { MenuDemandeComponent } from './component/MenuDemande/MenuDemande.component';
+import { ValidationComponent } from './component/validation/validation.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    AuthenticationComponent,
+    ValidationComponent,
+    ConsultationComponent,
+    NotificationComponent,
+    MainComponent,
+    CreationPrescripteurComponent,
+    CreationSession1Component,
+    TestComponent,
+    MenuDemandeComponent,
+    //  DetailComponent
+  ],
+  imports: [BrowserModule, HttpClientModule, AppRoutingModule, FormsModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
